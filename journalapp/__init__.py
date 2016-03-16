4fa1efde3fde7a0bef31b8bb9d32d8a190299e2c
Will Weatherford <weatherford.william@gmail.com>
2016-03-16 15:59:57 -0700

    updated init, setup and tox to use pyramid_jinja2

--- journalapp/__init__.py
+++ journalapp/__init__.py
@@ -1,21 +1,24 @@
+"""Initiliazes the journalapp in Pyramid."""
+import os
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
 from .models import (
     DBSession,
     Base,
-    )
+)
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
-    # read DB_NAME from os.environ; insert into settings
+    """Return a Pyramid WSGI application."""
+    database_url = os.environ.get('DATABASE_URL', None)
+    if database_url is not None:
+        settings['sqlalchemy.url'] = database_url
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
+    config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
