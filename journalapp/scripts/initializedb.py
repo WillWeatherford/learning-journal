db0055c68ac06ffe73125a50ac8d0f986aa424dd
GingerQ <qiangwenjing@gmail.com>
2016-03-15 18:07:15 -0700

    linter fix

--- journalapp/scripts/initializedb.py
+++ journalapp/scripts/initializedb.py
@@ -1,24 +1,26 @@
+"""Initialize DB."""
 import os
 import sys
-import transaction
+# import transaction
 
 from sqlalchemy import engine_from_config
 
 from pyramid.paster import (
     get_appsettings,
     setup_logging,
-    )
+)
 
 from pyramid.scripts.common import parse_vars
 
 from ..models import (
     DBSession,
-    MyModel,
     Base,
-    )
+    # Entry,
+)
 
 
 def usage(argv):
+    """Print usage to command line."""
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri> [var=value]\n'
           '(example: "%s development.ini")' % (cmd, cmd))
@@ -26,6 +28,7 @@ def usage(argv):
 
 
 def main(argv=sys.argv):
+    """Set up engine and session."""
     if len(argv) < 2:
         usage(argv)
     config_uri = argv[1]
