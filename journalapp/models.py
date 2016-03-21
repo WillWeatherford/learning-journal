ce788ebdb06163e186ed5cc54b684d8c9d3176c1
Will Weatherford <weatherford.william@gmail.com>
2016-03-21 09:09:40 -0700

    updated will-mockups and removed some unecessary lines from models.py

--- journalapp/models.py
+++ journalapp/models.py
@@ -9,14 +9,8 @@ from sqlalchemy import (
 )
 
 from sqlalchemy.ext.declarative import declarative_base
-
-from sqlalchemy.orm import (
-    scoped_session,
-    sessionmaker,
-)
-
+from sqlalchemy.orm import scoped_session, sessionmaker
 from zope.sqlalchemy import ZopeTransactionExtension
-
 import datetime
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
