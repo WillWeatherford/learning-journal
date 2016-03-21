d49a7683c7b28189230affb174072168e377f2ea
Will Weatherford <weatherford.william@gmail.com>
2016-03-20 22:42:46 -0700

    Removed try: except blocks for raising DBAPI 500 error.

--- journalapp/views.py
+++ journalapp/views.py
@@ -4,7 +4,6 @@ from pyramid.response import Response
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
 
-from sqlalchemy.exc import DBAPIError
 from .models import DBSession, Entry
 from journalapp.forms import EditEntryForm, AddEntryForm
 import markdown
@@ -13,65 +12,52 @@ import markdown
 @view_config(route_name='list', renderer='templates/list.jinja2')
 def list_view(request):
     """Return rendered list of entries for journal home page."""
-    try:
-        entries = DBSession.query(Entry).order_by(Entry.created.desc())
-        return {'entries': entries}
-    except DBAPIError:
-        return Response(DB_ERR_MSG, content_type='text/plain', status_int=500)
+    entries = DBSession.query(Entry).order_by(Entry.created.desc())
+    return {'entries': entries}
 
 
 @view_config(route_name='detail', renderer='templates/detail.jinja2')
 def detail_view(request):
     """Return rendered single entry for entry detail page."""
-    try:
-        entry_id = request.matchdict['entry_id']
-        # import pdb; pdb.set_trace()
-        entry = DBSession.query(Entry).get(entry_id)
-        if not entry:
-            return Response('Post {} does not exist.'.format(entry_id),
-                            content_type='text/plain',
-                            status_int=404)
-        return {'entry': entry}
-    except DBAPIError:
-        return Response(DB_ERR_MSG, content_type='text/plain', status_int=500)
+    entry_id = request.matchdict['entry_id']
+    entry = DBSession.query(Entry).get(entry_id)
+    if not entry:
+        return Response('Post {} does not exist.'.format(entry_id),
+                        content_type='text/plain',
+                        status_int=404)
+    return {'entry': entry}
 
 
 @view_config(route_name='add', renderer='templates/add-edit.jinja2')
 def add_entry(request):
     """Display a empty form, when submitted, return to the detail page."""
-    try:
-        form = AddEntryForm(request.POST)
-        if request.method == "POST" and form.validate():
-            new_entry = Entry(title=form.title.data, text=form.text.data)
-            DBSession.add(new_entry)
-            DBSession.flush()
-            next_url = request.route_url('detail', entry_id=new_entry.id)
-            return HTTPFound(location=next_url)
-        return {'form': form}
-    except DBAPIError:
-        return Response(DB_ERR_MSG, content_type='text/plain', status_int=500)
+    form = AddEntryForm(request.POST)
+    if request.method == "POST" and form.validate():
+        new_entry = Entry(title=form.title.data, text=form.text.data)
+        DBSession.add(new_entry)
+        DBSession.flush()
+        next_url = request.route_url('detail', entry_id=new_entry.id)
+        return HTTPFound(location=next_url)
+    return {'form': form}
 
 
 @view_config(route_name='edit', renderer='templates/add-edit.jinja2')
 def edit_entry(request):
     """Display editing page to edit entries, return to detail page."""
-    try:
-        entry_id = request.matchdict['entry_id']
-        entry = DBSession.query(Entry).get(entry_id)
-        if not entry:
-            return Response('Post {} does not exist.'.format(entry_id),
-                            content_type='text/plain',
-                            status_int=404)
-        form = EditEntryForm(request.POST, entry)
-        if request.method == "POST" and form.validate():
-            form.populate_obj(entry)
-            DBSession.add(entry)
-            DBSession.flush()
-            next_url = request.route_url('detail', entry_id=entry.id)
-            return HTTPFound(location=next_url)
-        return {'form': form}
-    except DBAPIError:
-        return Response(DB_ERR_MSG, content_type='text/plain', status_int=500)
+    entry_id = request.matchdict['entry_id']
+    entry = DBSession.query(Entry).get(entry_id)
+    if not entry:
+        return Response('Post {} does not exist.'.format(entry_id),
+                        content_type='text/plain',
+                        status_int=404)
+    form = EditEntryForm(request.POST, entry)
+    if request.method == "POST" and form.validate():
+        form.populate_obj(entry)
+        DBSession.add(entry)
+        DBSession.flush()
+        next_url = request.route_url('detail', entry_id=entry.id)
+        return HTTPFound(location=next_url)
+    return {'form': form}
 
 
 def render_markdown(content, linenums=False, pygments_style='default'):
